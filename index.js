822c7b5084224d9f6eeb8c7ea9965685332fda3c
cseng3 <cseng214@gmail.com>
2017-06-16 20:54:28 -0400

    Added test assistant action

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+'use strict';
+
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+
+process.env.DEBUG = 'actions-on-google:*';
+const Assistant = require('actions-on-google').ApiAiAssistant;
+
+exports.testAction = (req, res) => {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Fulfill action business logic
+  function responseHandler (assistant) {
+    // Complete your fulfillment logic and send a response
+    assistant.tell('Hello, World!');
+  }
+
+  const actionMap = new Map();
+  actionMap.set('testAction', responseHandler);
+
+  assistant.handleRequest(actionMap);
+};
 
 app.set('port', (process.env.PORT || 5000));
 
